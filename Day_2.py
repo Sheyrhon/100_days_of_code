572383fa094d0cd1d0229da6d1384d560b02ad7a
sheyrhon <chisheyrhon96@gmail.com>
2022-12-06 16:23:01 +0100

    tip calculator

---
+++ Day_2.py
@@ -0,0 +1,3 @@
+print("Welcome to this tip calculator")
+tip = float(input("What is the total bill $"))
+tip_percentage = int(input(print("What percentage tip would you like to give ? 10, 12, or 15, 12")))
