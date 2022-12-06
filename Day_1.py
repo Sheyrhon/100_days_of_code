e465801ec4c6aa8e30000a494bb819832e977039
sheyrhon <chisheyrhon96@gmail.com>
2022-12-06 16:07:50 +0100

    worked on tip calculator

---
+++ Day_1.py
@@ -0,0 +1,6 @@
+#Band Name generator
+
+print("Welcome to the Band Name Generator")
+name= input("What\' s the name of the city you grew up in?\n")
+pet= input("What\'s your pet\'s name?\n")
+print(f"Your Band name could be {name} {pet} ")
